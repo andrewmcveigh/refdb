5adb3eecc2ece96b91f498c224d2839b3c640b09
Andrew Mcveigh <me@andrewmcveigh.com>
2014-09-24 16:13:46 +0200

    Bump to 0.2.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.andrewmcveigh/refdb "0.2.3"
+(defproject com.andrewmcveigh/refdb "0.2.4-SNAPSHOT"
   :description "File-backed ref-based \"database\""
   :url "http://github.com/andrewmcveigh/refdb"
   :dependencies [[org.clojure/clojure "1.6.0"]
