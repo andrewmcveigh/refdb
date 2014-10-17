7fb9c252665a8b5c412f3a03614da7ee03f6ebd0
Andrew Mcveigh <me@andrewmcveigh.com>
2014-10-17 17:07:10 +0200

    Add license to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject com.andrewmcveigh/refdb "0.4.0-SNAPSHOT"
   :description "File-backed ref-based \"database\""
   :url "http://github.com/andrewmcveigh/refdb"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [riddley "0.1.7"]]
   :profiles {:dev {:plugins [[lein-marginalia "0.7.1"]]}})
