a1be5ace41c10c4ef10910f2be8d8cd575da105b
Andrew Mcveigh <me@andrewmcveigh.com>
2014-11-04 19:13:14 +0100

    Prepare release: Add auto-release conf

--- project.clj
+++ project.clj
@@ -5,8 +5,23 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [riddley "0.1.7"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[com.andrewmcveigh/lein-auto-release "0.1.5"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["auto-release" "checkout" "master"]
+                  ["auto-release" "merge-no-ff" "develop"]
+                  ["change" "version"
+                   "leiningen.release/bump-version" "release"]
+                  ["auto-release" "update-release-notes"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v"]
+                  ["deploy" "clojars"]
+                  ["vcs" "push"]
+                  ["auto-release" "checkout" "develop"]
+                  ["auto-release" "merge" "master"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :profiles {:dev {:plugins [[lein-marginalia "0.7.1"]]}
              :test {:dependencies [[prismatic/schema "0.3.1"]]}}
   :aliases {"deploy" ["do" ["clean"] ["test"] ["deploy" "clojars"]]})
